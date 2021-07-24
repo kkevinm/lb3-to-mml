ea1cec28ae0c254c5d075f1969e65c937a4c723d
Kevin M <kevin_495@hotmail.it>
2021-07-24 12:28:51 +0200

    Fixed pom and added basic app parameter handle

--- src/main/java/com/github/kevinm/lb3tomml/Main.java
+++ src/main/java/com/github/kevinm/lb3tomml/Main.java
@@ -2,7 +2,6 @@ package com.github.kevinm.lb3tomml;
 
 import java.io.IOException;
 
-import com.github.kevinm.lb3tomml.spc.Aram;
 import com.github.kevinm.lb3tomml.spc.Spc;
 import com.github.kevinm.lb3tomml.spc.SpcException;
 
@@ -13,22 +12,23 @@ public final class Main {
     }
 
     public static void main(String[] args) {
-        String file = "./05 Light.spc";
+        // Just for quick testing
+        args = new String[1];
+        args[0] = "./05 Light.txt";
+        //
+        
+        if (args.length < 1) {
+            System.out.println("Error: not enough parameters!");
+            System.out.println("Usage: java -jar lib3-to-mml.jar <spc file>");
+            return;
+        }
+        
+        Spc spc;
         try {
-            Spc spc = Spc.loadSpc(file);
-            Spc.Id666Tag tag = spc.id666Tag.get();
-            System.out.println(tag.gameTitle);
-            System.out.println(tag.artistName);
-            System.out.println(tag.dumpDate);
-            Aram ram = Aram.fromSpc(spc);
-            System.out.println(ram.getSignedByte(0));
-            System.out.println(ram.getSignedWord(0));
-            System.out.println(ram.getSignedLong(0));
-            System.out.println(ram.getUnsignedByte(0));
-            System.out.println(ram.getUnsignedWord(0));
-            System.out.println(ram.getUnsignedLong(0));
+            spc = Spc.loadSpc(args[0]);
         } catch (IOException | SpcException e) {
-            e.printStackTrace();
+            System.out.println("Error: " + e.getLocalizedMessage());
+            return;
         }
     }
 
